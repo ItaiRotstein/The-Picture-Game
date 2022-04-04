19e3113ab89df5f04bacd0b209cd5b90ef79781b
Itai Rotstein <itairotstein@gmail.com>
2022-04-04 14:44:30 +0300

    First Commit

---
+++ js/main.js
@@ -0,0 +1,105 @@
+'use strict'
+
+var gOpts = [
+    ['This place is in Rome', 'This place is in Paris', 0],
+    ['This is a plane', 'This is a car', 1],
+    ['These are presidents', 'These are football players', 0]
+]
+
+var gNumQuests = 3
+var gQuests
+
+var gIsCorrect = false
+var gCurrQuestIdx = 0
+var gNextId = 0
+
+function initGame() {
+    gQuests = createQuests(gNumQuests)
+    renderQuest()
+}
+
+function createQuests(num) {
+
+    var questions = []
+    for (var i = 0; i < num; i++) {
+        var question = {
+            id: gNextId++,
+            opts: gOpts[i],
+            correctOptIdx: gOpts[i][2]
+        }
+        questions.push(question)
+        // console.log(questions);
+    }
+    return questions
+}
+
+function nextQuest() {
+
+    var elBtnNextQuest = document.querySelector('button')
+    var elMsgCorrect = document.querySelector('.msgCorrect')
+
+    elMsgCorrect.style.display = 'none'
+    elBtnNextQuest.style.display = 'none'
+
+    gIsCorrect = false
+
+    renderQuest()
+}
+
+function renderQuest() {
+    var elAnswers = document.querySelector('.answers')
+    var img = document.querySelector('img')
+    img.src = `img/${gCurrQuestIdx}.jpg`
+    var strHTML = ''
+    
+    for (var i = 0; i < gQuests[gCurrQuestIdx].opts; i++) {
+        strHTML += `<div class="answer" data-opt="${i}" onclick="checkAnswer(this)">${gQuests[gCurrQuestIdx].opts[i]}</div>`
+    }
+    
+
+
+    // for (var i = 0; i < gQuests.length; i++) {
+    //     if (gCurrQuestIdx === i) {
+    //         strHTML += `<img src="img/${i + 1}.jpg"><div class="answers"><div class="answer" data-opt="0" onclick="checkAnswer(this)">${gQuests[i].opts[0]}</div><div class="answer" data-opt="1" onclick="checkAnswer(this)">${gQuests[i].opts[1]}</div></div>`
+    //     }
+    // }
+    elAnswers.innerHTML = strHTML
+}
+
+
+function checkAnswer(optIdx) {
+    var elMsgCorrect = document.querySelector('.msgCorrect')
+    var elBtnNextQuest = document.querySelector('button')
+    var elAnswers = document.querySelector('.answers')
+    var elH1 = document.querySelector('h1')
+    for (var i = 0; i < gQuests.length; i++) {
+        if (i === gCurrQuestIdx) {
+            var correctOptIdx = gQuests[i].correctOptIdx
+            console.log(correctOptIdx);
+
+            if (+optIdx.dataset.opt === correctOptIdx) {
+                elMsgCorrect.style.display = 'inline'
+                elBtnNextQuest.style.display = 'inline'
+                elAnswers.style.display = 'none'
+                if (!gIsCorrect) {
+                    gCurrQuestIdx++
+                    gIsCorrect = true
+                }
+                if (gCurrQuestIdx === gNumQuests) {
+                    printVictory()
+                    elH1.style.display = 'none'
+                }
+            }
+        }
+    }
+}
+
+function printVictory() {
+    var elMsgCorrect = document.querySelector('.msgCorrect')
+    var elBtnNextQuest = document.querySelector('button')
+    var gameSpace = document.querySelector('.game')
+
+    elMsgCorrect.style.display = 'none'
+    elBtnNextQuest.style.display = 'none'
+    gameSpace.innerHTML = `<h1>CONGRATULATIONS!! YOU WON!!!</h1>`
+}   
